b016b48633ac1583f0e6f3ba9ec5ec1f245767b1
AlexanderZon <theguitarplayer.am@gmail.com>
2015-03-07 01:15:07 -0430

    rock paper scissors lizard spock

--- app/routes.php
+++ app/routes.php
@@ -11,8 +11,53 @@
 |
 */
 
+Route::get('/rpsls',function(){
+	$args = array(
+		array(
+			'question' => '¿De que color es el caballo de Simón Bolívar?',
+			'answer'=> 3,
+			'options' => array(
+				array(
+					'id' => 0,					
+					'item' => "rock",
+					'name' => "Rojo",
+					'answer' => false,
+					),
+				array(
+					'id' => 1,					
+					'item' => "paper",
+					'name' => "Azul",
+					'answer' => false,
+					),
+				array(
+					'id' => 2,					
+					'item' => "scisors",
+					'name' => "Verde",
+					'answer' => false,
+					),
+				array(
+					'id' => 3,					
+					'item' => "lizard",
+					'name' => "Blanco",
+					'answer' => false,
+					),
+				array(
+					'id' => 4,					
+					'item' => "spock",
+					'name' => "Amarillo",
+					'answer' => false,
+					),
+				)
+			)
+		);
+
+	return Response::json( $args );
+	
+});
+
 Route::get('/', function()
 {
 	$lesson = Lesson::find(1);
 	dd($lesson->attachments[0]);
 });
+
