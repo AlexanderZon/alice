92081765200a076fdfa8901f6cc64415e12a9881
AlexanderZon <theguitarplayer.am@gmail.com>
2015-06-07 11:29:22 -0430

    show courses view

--- app/views/teachers/courses/show.blade.php
+++ app/views/teachers/courses/show.blade.php
@@ -33,19 +33,9 @@
 									<div class="row">
 										<div class="col-md-6">
 											<div class="form-group">
-												<label class="control-label col-md-3">Nombre</label>
+												<label class="control-label col-md-3">Nombre del Curso</label>
 												<div class="col-md-9">
-													<input name="first_name" type="text" class="form-control" placeholder="Ingrese el nombre del Usuario" value="{{ $course->first_name }}">
-													<!-- <span class="help-block">This is inline help</span> -->
-												</div>
-											</div>
-										</div>
-										<!--/span-->
-										<div class="col-md-6">
-											<div class="form-group {{ false ? 'has-error' : '' }}">
-												<label class="control-label col-md-3">Apellido</label>
-												<div class="col-md-9">
-													<input name="last_name" type="text" class="form-control" placeholder="Ingrese el nombre del Usuario" value="{{ $course->last_name }}">
+													<input name="first_name" type="text" class="form-control" placeholder="Ingrese el nombre del Curso" value="{{ $course->title }}">
 													<!-- <span class="help-block">This is inline help</span> -->
 												</div>
 											</div>
@@ -96,26 +86,6 @@
 														Remover </a>
 													</div>
 												</div>
-												<!-- <div class="fileinput fileinput-new" data-provides="thumbnail_picture">
-													<div>Imagen Miniatura</div>
-													<div class="fileinput-new thumbnail" style="width: 100px; height: 100px;">
-														<img src="http://www.placehold.it/200x150/EFEFEF/AAAAAA&amp;text=no+image" alt=""/>
-														<img src="{{ $course->thumbnail_picture != '' ? $course->thumbnail_picture : Course::DEFAULT_THUMBNAIL_PICTURE }}" alt=""/>
-													</div>
-													<div class="fileinput-preview fileinput-exists thumbnail" style="max-width: 200px; max-height: 200px;">
-													</div>
-													<div>
-														<span class="btn default btn-file">
-														<span class="fileinput-new">
-														Seleccionar Imagen </span>
-														<span class="fileinput-exists">
-														Cambiar </span>
-														<input type="file" name="thumbnail_picture">
-														</span>
-														<a href="javascript:;" class="btn red fileinput-exists" data-dismiss="thumbnail_picture">
-														Remover </a>
-													</div>
-												</div> -->
 											</div>
 										</div>
 										<!--/span-->
@@ -123,60 +93,15 @@
 									<div class="row">
 										<div class="col-md-12">&nbsp;</div>
 									</div>
-									<div class="row">
-										<div class="col-md-6">
-											<div class="form-group {{ $msg_warning['name'] == 'users_profile_born_date_err' ? 'has-error' : '' }}">
-												<label class="control-label col-md-3">Fecha de Nacimiento</label>
-												<div class="col-md-9">
-													<div class="input-group input-medium date date-picker" data-date-format="dd-mm-yyyy" data-date-end-date="+0d">
-														<input name="born_date" type="text" placeholder="Ingrese la Fecha de Nacimiento" class="form-control"  value="{{ date('d-m-Y', strtotime($course->born_date)) }}" required readonly>
-														<soan class="input-group-btn">
-															<button class="btn default" type="button"><i class="fa fa-calendar"></i></button>
-														</soan>
-													</div>
-												</div>
-											</div>
-										</div>
-										<div class="col-md-6">
-											<div class="form-group {{ $msg_warning['name'] == 'users_profile_born_place_err' ? 'has-error' : '' }}">
-												<label class="control-label col-md-3">Lugar de Nacimiento</label>
-												<div class="col-md-9">
-													<div class="input-icon right">
-														<i class="fa fa-map-marker"></i>
-														<input name="born_place" type="text" placeholder="Ingrese el Correo Electrónico" class="form-control"  value="{{ $course->born_place }}">
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-									<div class="row">
-										<div class="col-md-6">
-											<div class="form-group {{ $msg_warning['name'] == 'users_profile_sex_err' ? 'has-error' : '' }}">
-												<label class="control-label col-md-3">Sexo</label>
-												<div class="col-md-9">
-													<div class="input-group ">
-														<span class="input-group-addon">
-														<i class="fa fa-user"></i>
-														</span>
-														<select name="sex" class="form-control select2me" data-placeholder="Seleccione un rol de usuario...">
-															<option value>--- SELECCIONE UN SEXO ---</option>
-															<option value="male" {{ $course->sex == 'male' ? 'selected' : '' }}>Masculino</option>
-															<option value="female" {{ $course->sex == 'female' ? 'selected' : '' }}>Femenino</option>
-														</select>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
 
-									<h3 class="form-section">Sobre Mí</h3>
+									<h3 class="form-section">Descripción</h3>
 									<!--/row-->
 									<div class="row">
 										<div class="col-md-12">
-											<div class="form-group {{ $msg_warning['name'] == 'users_profile_about_me_err' ? 'has-error' : '' }}">
+											<div class="form-group {{ $msg_warning['name'] == 'teachers_courses_show_description_err' ? 'has-error' : '' }}">
 												<div class="col-md-12">
-													<textarea class="col-md-12 summernote" name="about_me">
-														{{ $course->about_me }}
+													<textarea class="col-md-12 summernote" name="description">
+														{{ $course->description }}
 													</textarea>
 													<!-- <span class="help-block">This is inline help</span> -->
 												</div>
@@ -184,169 +109,6 @@
 										</div>
 										<!--/span-->
 									</div>
-
-									<h3 class="form-section">Actividades</h3>
-									<!--/row-->
-									<div class="row">
-										<div class="col-md-12">
-											<div class="form-group {{ $msg_warning['name'] == 'users_profile_activities_err' ? 'has-error' : '' }}">
-												<div class="col-md-12">
-													<textarea class="col-md-12 summernote" name="activities">
-														{{ $course->activities }}
-													</textarea>
-													<!-- <span class="help-block">This is inline help</span> -->
-												</div>
-											</div>
-										</div>
-										<!--/span-->
-									</div>
-
-									<h3 class="form-section">Intereses</h3>
-									<!--/row-->
-									<div class="row">
-										<div class="col-md-12">
-											<div class="form-group {{ $msg_warning['name'] == 'users_profile_interests_err' ? 'has-error' : '' }}">
-												<div class="col-md-12">
-													<textarea class="col-md-12 summernote" name="interests">
-														{{ $course->interests }}
-													</textarea>
-													<!-- <span class="help-block">This is inline help</span> -->
-												</div>
-											</div>
-										</div>
-										<!--/span-->
-									</div>
-									<h3 class="form-section">Información de Usuario</h3>
-									<!--/row-->
-									<div class="row">
-										<div class="col-md-6">
-											<div class="form-group {{ $msg_warning['name'] == 'users_profile_username_err' ? 'has-error' : '' }}">
-												<label class="control-label col-md-3">Usuario</label>
-												<div class="col-md-9">
-													<div class="input-icon right">
-														<i class="fa fa-user"></i>
-														<input name="username" type="text" class="form-control" placeholder="Ingrese el nombre del Usuario" value="{{ $course->username }}" readonly required>
-													</div>
-													<!-- <span class="help-block">This is inline help</span> -->
-												</div>
-											</div>
-										</div>
-										<!--/span-->
-										<div class="col-md-6">
-											<div class="form-group">
-												<label class="control-label col-md-3">Nombre para mostrar</label>
-												<div class="col-md-9">
-													<div class="input-icon right">
-														<i class="fa fa-eye"></i>
-														<input name="display_name" type="text" class="form-control" placeholder="Ingrese el nombre para mostrar" value="{{ $course->display_name }}">
-													</div>
-													<!-- <span class="help-block">This is inline help</span> -->
-												</div>
-											</div>
-										</div>
-										<!--/span-->
-									</div>
-									<!--/row-->
-									<div class="row">
-										<div class="col-md-6">
-											<div class="form-group {{ $msg_warning['name'] == 'users_profile_email_err' ? 'has-error' : '' }}">
-												<label class="control-label col-md-3">Correo</label>
-												<div class="col-md-9">
-													<div class="input-icon right">
-														<i class="fa fa-envelope"></i>
-														<input name="email" type="email" placeholder="Ingrese el Correo Electrónico" class="form-control"  value="{{ $course->email }}" required>
-													</div>
-												</div>
-											</div>
-										</div>
-										<div class="col-md-6">
-											<div class="form-group {{ $msg_warning['name'] == 'users_profile_website_err' ? 'has-error' : '' }}">
-												<label class="control-label col-md-3">Website</label>
-												<div class="col-md-9">
-													<div class="input-icon right">
-														<i class="fa fa-globe"></i>
-														<input name="website" type="text" placeholder="Ingrese la URL de su Página Web" class="form-control"  value="{{ $course->website }}">
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-									<div class="row">
-										<div class="col-md-6">
-											<div class="form-group {{ $msg_warning['name'] == 'users_profile_facebook_err' ? 'has-error' : '' }}">
-												<label class="control-label col-md-3">Facebook</label>
-												<div class="col-md-9">
-													<div class="input-icon right">
-														<i class="fa fa-facebook"></i>
-														<input name="facebook" type="text" placeholder="Ingrese su Nombre de Usuario en Facebook" class="form-control"  value="{{ $course->facebook }}">
-													</div>
-												</div>
-											</div>
-										</div>
-										<div class="col-md-6">
-											<div class="form-group {{ $msg_warning['name'] == 'users_profile_twitter_err' ? 'has-error' : '' }}">
-												<label class="control-label col-md-3">Twitter</label>
-												<div class="col-md-9">
-													<div class="input-icon right">
-														<i class="fa fa-twitter"></i>
-														<input name="twitter" type="text" placeholder="Ingrese su Nombre de Usuario en Twitter" class="form-control"  value="{{ $course->twitter }}">
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-									<div class="row">
-										<div class="col-md-6">
-											<div class="form-group {{ $msg_warning['name'] == 'users_profile_address_err' ? 'has-error' : '' }}">
-												<label class="control-label col-md-3">Direccion</label>
-												<div class="col-md-9">
-													<div class="input-icon right">
-														<i class="fa fa-map-marker"></i>
-														<input name="address" type="text" placeholder="Ingrese su Dirección" class="form-control"  value="{{ $course->address }}">
-													</div>
-												</div>
-											</div>
-										</div>
-										<div class="col-md-6">
-											<div class="form-group {{ $msg_warning['name'] == 'users_profile_phones_err' ? 'has-error' : '' }}">
-												<label class="control-label col-md-3">Teléfono</label>
-												<div class="col-md-9">
-													<div class="input-icon right">
-														<i class="fa fa-phone"></i>
-														<input name="phones" type="text" placeholder="Ingrese su Número de Teléfono" class="form-control"  value="{{ $course->phones }}">
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-									<h3 class="form-section">Cambiar Contraseña</h3>
-									<div class="row">
-										<div class="col-md-6">
-											<div class="form-group {{ $msg_warning['name'] == 'users_profile_password_err' ? 'has-error' : '' }}">
-												<label for="inputPassword1" class="col-md-3 control-label">Contraseña</label>
-												<div class="col-md-9">
-													<div class="input-icon right">
-														<i class="fa fa-lock"></i>
-														<input name="password_1" type="password" class="form-control" id="inputPassword1" placeholder="Ingrese la Nueva Contraseña">
-													</div>
-												</div>
-											</div>
-										</div>
-										<!--/span-->
-										<div class="col-md-6">
-											<div class="form-group {{ $msg_warning['name'] == 'users_profile_password_err' ? 'has-error' : '' }}">
-												<label for="inputPassword1" class="col-md-3 control-label">Repita la Contraseña</label>
-												<div class="col-md-9">
-													<div class="input-icon right">
-														<i class="fa fa-lock"></i>
-														<input name="password_2" type="password" class="form-control" id="inputPassword1" placeholder="Ingrese de nuevo la Nueva Contraseña">
-													</div>
-												</div>
-											</div>
-										</div>
-										<!--/span-->
-									</div>
-									<!--/row-->
 								</div>
 								<div class="form-actions">
 									<div class="row">
