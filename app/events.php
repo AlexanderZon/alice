a95c62dd9602b29f1ac60734efe0497630993bec
AlexanderZon <theguitarplayer.am@gmail.com>
2015-07-29 20:01:19 -0430

    notifications for new students adapter

--- app/events.php
+++ app/events.php
@@ -2,6 +2,26 @@
 
 	/* SOME EVENTS PUT HERE */
 
-	Event::listen('notification.new_user', function(){
-		return false;
+	Event::listen('notification.new_student', function($student){
+
+		$coordinators = User::getCoordinators();
+
+		foreach($coordinators as $coordinator):
+
+			Notification::create(array(
+				'student_id' => $coordinator->id,
+				'notificationable_id' => $student->id,
+				'notificationable_type' => 'User',
+				'icon' => 'fa-plus',
+				'picture' => $student->profile->getAvatar(),
+				'route' => '/coordinators/students/inactive',
+				'title' => 'Nuevo Estudiante Registrado',
+				'description' => $student->last_name.' '.$student->first_name.' se ha registrado como '.$user->username,
+				'status' => 'fired'
+				));
+
+		endforeach;
+
+		return true;
+		
 	});
